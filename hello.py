924503d7dbd23cbbf4af2772733a143d8300ad89
Yago Calabuig <yago.calabuig@gmail.com>
2023-02-21 18:51:15 +0100

    yago

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 def antonio():
     print("Hola soy Antonio, vuestro profesor.")
 
+def yago():
+    print("Hola soy Pepe, xd.")
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
+    yago()
