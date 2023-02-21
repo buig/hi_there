41a46f19e9e2a716d7372e958338ef6304cb3b91
Alvaro Arellano <alvareaya@alu.edu.gva.es>
2023-02-21 19:05:48 +0100

    Alvaro parte

--- hello.py
+++ hello.py
@@ -10,9 +10,13 @@ def isaac():
 def maria():
     print("Hola soy Maria.")
 
+def alvaro():
+    print("Hola soy Alvaro")
+
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     yago()
     isaac()
     maria()
+    alvaro()
